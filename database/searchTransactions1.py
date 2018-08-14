0be63cd2ac8463de5657886e93b73b1b4e9f57cb
Nieland <janieland@deloitte.nl>
2018-08-14 16:39:58 +0200

    added two ways to search through all transactions for moon transactions

---
+++ database/searchTransactions1.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import time
+import os
+
+# time keeping ready?
+startTime = time.process_time_ns()
+
+# get list of all files
+files = os.listdir("database/output")
+
+# initialize
+transactionsSend = pd.DataFrame()
+transactionsReceived = pd.DataFrame()
+
+# loop through all files
+for filename in files:
+    # read the file
+    filepath = "database/output/" + filename
+    transactionsLeg = pd.read_csv(filepath, sep=';')
+
+    # append to the receiver frame if receiver file
+    if(filename.startswith("receiver")):
+        transactionsReceived = pd.concat([transactionsReceived, transactionsLeg], ignore_index=True)
+
+    # append to sender frame if sender file
+    elif(filename.startswith("sender")):
+        transactionsSend = pd.concat([transactionsSend, transactionsLeg], ignore_index=True)
+
+
+# join the sender and receiver data frames
+transactions = transactionsSend.join(transactionsReceived, on="transactionID", lsuffix="send", rsuffix="rec")
+
+# find the transactions to the moon
+toTheMoon = (transactions.receiverCountryISO == "TMN").values
+
+# select only the moon transactions
+transactionsToTheMoon = transactions.loc[toTheMoon]
+
+# stopping time
+finishTime = time.process_time_ns()
+print(f'Executed sequence in {(finishTime-startTime)/1000000} milliseconds')
+print(transactionsToTheMoon)
---
+++ database/searchTransactions2.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import time
+import os
+
+# time keeping ready?
+startTime = time.process_time_ns()
+
+# get list of all files
+files = os.listdir("database/output")
+
+# initialize
+transactionsToTheMoon = pd.DataFrame()
+
+# loop through all files
+for filename in files:
+
+    # check if the file contains the receiver part
+    if(filename.startswith("receiver")):
+        # read file in
+        filepathSend = "database/output/" + filename        # build full path
+        receiverLeg = pd.read_csv(filepathSend, sep=';')      # actual read in
+
+        # check if there are any transactions from IRAN
+        transactionInScope = receiverLeg.receiverCountryISO == 'TMN'
+        if(sum(transactionInScope) > 0):
+            # select transactions to the moon
+            transactionsRec = receiverLeg[transactionInScope]
+
+            # read receiver file
+            filepathSend = filepathSend.replace("receiver", "sender")
+            senderLeg = pd.read_csv(filepathSend, sep=";")
+
+            transactionsToTheMoon = pd.concat([
+                transactionsToTheMoon,
+                pd.merge(transactionsRec, senderLeg, on="transactionID", how="left")
+            ], ignore_index=True)
+
+
+# stop time keeping
+finishTime = time.process_time_ns()
+print(f'Executed sequence in {(finishTime-startTime)/1000000} milliseconds')
+print(transactionsToTheMoon)
